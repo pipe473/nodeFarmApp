7ec44c1c6dece43d3eb5bfca2f8da9f4959dc25e
Andres Felipe Bedoya Guzman <andresfelipebedoyaguzman@mbp-de-andres.intern>
2019-11-16 11:05:59 +0100

    It has been using Blocking form with a Synchronous way

---
+++ index.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+
+// Blocking, Synchronous way
+const textIn = fs.readFileSync('./txt/input.txt','utf-8');
+
+console.log(textIn);
+
+const textOut = `Este texto es de prueba: ${textIn}.\nCreated on ${Date.now()} `;
+fs.writeFileSync('./txt/output.txt', textOut);
+console.log('File written...!');
