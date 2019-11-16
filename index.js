1b8ee72e047257afcd6768f414241d12aeb170eb
Andres Felipe Bedoya Guzman <andresfelipebedoyaguzman@mbp-de-andres.intern>
2019-11-16 11:40:31 +0100

    First steps using Asynchronous file

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 const fs = require('fs');
 
-// Blocking, Synchronous way
-const textIn = fs.readFileSync('./txt/input.txt','utf-8');
+// // Blocking, Synchronous way
+// const textIn = fs.readFileSync('./txt/input.txt','utf-8');
 
-console.log(textIn);
+// console.log(textIn);
 
-const textOut = `Este texto es de prueba: ${textIn}.\nCreated on ${Date.now()} `;
-fs.writeFileSync('./txt/output.txt', textOut);
-console.log('File written...!');
+// const textOut = `Este texto es de prueba: ${textIn}.\nCreated on ${Date.now()} `;
+// fs.writeFileSync('./txt/output.txt', textOut);
+// console.log('File written...!');
+
+
+// Non Blocking, Asynchronous way
+fs.readFile('./txt/start.txt', 'utf-8', (err, data1) =>{
+    fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) =>{
+        console.log(data2);
+        
+    });
+    
+});
+
+console.log('Will read file!');
